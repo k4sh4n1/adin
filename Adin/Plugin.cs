637f01b388bef1df7f3b4c129d2bce60ead0bec5
k4sh4n1 <kashani@alum.sharif.edu>
2024-11-12 04:55:58 -0800

    Hello world

---
+++ Adin/Plugin.cs
@@ -0,0 +1,48 @@
+﻿using KD.SDK2;
+
+using System.Windows.Forms;
+
+using System.IO;
+using System.Reflection;
+
+namespace Adin
+{
+    public class Plugin
+    {
+        Appli _appli;
+        Appli.MenuItem _myMenuItem;
+
+        public Plugin()
+        {
+            _appli = new Appli();
+        }
+
+        public bool OnPluginLoad(int unused)
+        {
+            var menuInfo = new Appli.MenuItemInsertionInfo();
+
+            menuInfo.Text = "Hello, world!";
+            menuInfo.DllFilenameWithoutPath = Path.GetFileName(Assembly.GetExecutingAssembly().Location);
+            menuInfo.ClassName = nameof(Plugin);
+            menuInfo.MethodName = nameof(OnMyMenuItem);
+
+            _myMenuItem = _appli.InsertMenuItem(menuInfo, Appli.MenuItem.StandardId.Help_LoadHours);
+
+            return true;
+        }
+
+        public bool OnPluginUnload(int unused)
+        {
+            _appli.RemoveMenuItem(_myMenuItem);
+
+            return true;
+        }
+
+        public bool OnMyMenuItem(int unused)
+        {
+            MessageBox.Show("Hello, world!");
+
+            return true;
+        }
+    }
+}
